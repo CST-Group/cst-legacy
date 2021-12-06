58855fc0c0e94af11c5dd999428cc6b76738c345
Wandemberg Gibaut <wandemberg.gibaut@eldorado.org.br>
2021-12-06 15:39:29 -0300

    solving some issues

---
+++ src/test/java/br/unicamp/cst/behavior/subsumption/TestDummy.java
@@ -0,0 +1,21 @@
+package br.unicamp.cst.behavior.subsumption;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Test;
+
+/**
+ * @author Wandemberg Gibaut
+ *
+ */
+public class TestDummy {
+
+    @Test
+    public void dummyTest(){
+        boolean dummy = true;
+
+        assertTrue(dummy);
+        
+    }
+
+}
